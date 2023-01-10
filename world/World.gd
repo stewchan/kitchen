254e681c6e1a549589cb92118b9c9317ac624da1
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-01-10 17:37:42 -0500

    Random spawn ingredients and order

--- world/World.gd
+++ world/World.gd
@@ -3,53 +3,62 @@ extends Node2D
 
 signal order_added(order)
 
-
 var score: int = 0
+var order_count: int = 0
+var ingredient_count: int = 0
 
+var IngredientScene = preload("res://kitchen/draggable/ingredient/Ingredient.tscn")
+var OrderScene = preload("res://kitchen/servery/order/Order.tscn")
+var DishScene = preload("res://kitchen/servery/dish/Dish.tscn")
 
 onready var players = $Players
 onready var servery = $Servery
 onready var hud = $HUD
 onready var orders = $HUD/H/Orders
 
-var IngredientScene = preload("res://kitchen/draggable/ingredient/Ingredient.tscn")
-var OrderScene = preload("res://kitchen/servery/order/Order.tscn")
-var DishScene = preload("res://kitchen/servery/dish/Dish.tscn")
-
 
 func _ready() -> void:
+	randomize()
 	servery.connect("served", self, "on_dish_served")
 	
+	spawn_ingredient()
+	spawn_ingredient()
+	spawn_ingredient()
 	
-	var ingredient1 = IngredientScene.instance()
-	ingredient1.set_name("tomato")
-	ingredient1.position = Vector2(200, 300)
-	add_child(ingredient1)
-	
-	var ingredient2 = IngredientScene.instance()
-	ingredient2.set_name("lettuce")
-	ingredient2.position = Vector2(800,400)
-	add_child(ingredient2)
-	
-	var ingredient3 = IngredientScene.instance()
-	ingredient3.set_name("lettuce")
-	ingredient3.position = Vector2(600,100)
-	add_child(ingredient3)
-	
+	spawn_order()
+	spawn_order()
+	spawn_order()
+
+
+func spawn_order() -> void:
 	var order = OrderScene.instance()
 	orders.add_child(order)
 	var dish = DishScene.instance()
-	dish.ingredients = ["tomato"]#, "lettuce"]
+	var r = randi() % 3
+	if r == 1:
+		dish.ingredients = ["tomato"]
+	elif r == 2:
+		dish.ingredients = ["lettuce"]
+	else:
+		dish.ingredients = ["eggplant"]
 	order.set_dish(dish)
 	emit_signal("order_added", order)
 
-	var order2 = OrderScene.instance()
-	orders.add_child(order2)
-	var dish2 = DishScene.instance()
-	dish2.ingredients = ["lettuce"]#, "lettuce"]
-	order2.set_dish(dish2)
-	emit_signal("order_added", order2)
-	
+
+func spawn_ingredient() -> void:
+	var r = randi() % 3
+	var ingredient = IngredientScene.instance()
+	ingredient_count += 1
+	ingredient.name = "ingredient" + str(ingredient_count)
+	if r == 1:
+		ingredient.set_name("tomato")
+	elif r == 2:
+		ingredient.set_name("tomato")
+	else:
+		ingredient.set_name("eggplant")
+	ingredient.position = Vector2(ingredient_count * 200, 450)
+	add_child(ingredient)
+
 
 func on_dish_served(dish: Dish) -> void:
 	for order in orders.get_children():
